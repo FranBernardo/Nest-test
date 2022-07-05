38ae4385b82fb459526bb772786ca5521e601dd3
Franciele <francielebernardo88@hotmail.com>
2022-07-05 11:35:11 -0300

    feat(novo/metodo): adicionando novos metodos

--- src/pessoas/PessoasController.ts
+++ src/pessoas/PessoasController.ts
@@ -1,9 +1,24 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 @Controller('cadastro/pessoas')
 export class PessoasController {
   @Get()
   todosUsuarios(): string {
-    return 'primeiro metodo';
+    return 'Mostra todos usuarios';
+  }
+
+  @Post()
+  cadastro(@Body() pessoas): string {
+    return `novo criar cadastro ${pessoas}`
+  }
+
+  @Put()
+  editar(@Body() pessoas): string {
+    return `editar cadastro ${pessoas   }`
+  }
+
+  @Delete(':id')
+  delete(@Param() pessoas): string {
+    return `Deletar do banco ${pessoas.id}`
   }
 }
