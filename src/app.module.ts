fc84b60f25d3372113657f70b38ada39e9ac2d11
Franciele <francielebernardo88@hotmail.com>
2022-07-08 09:44:52 -0300

    feat(add:model e service): adicionando model e separando o service de controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PessoaService } from './pessoas/Pessoa.service';
 import { PessoasController } from './pessoas/PessoasController';
 
 @Module({
   imports: [],
   controllers: [AppController, PessoasController],
-  providers: [AppService],
+  providers: [AppService, PessoaService],
 })
 export class AppModule {}
--- src/pessoas/PessoasController.ts
+++ src/pessoas/PessoasController.ts
@@ -1,24 +1,36 @@
 /* eslint-disable prettier/prettier */
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { PessoaService } from './Pessoa.service';
+import { Pessoa } from './Pessoa.model';
 @Controller('cadastro/pessoas')
 export class PessoasController {
+  constructor(private pessoaService: PessoaService){
+
+  }
+
   @Get()
-  todosUsuarios(): string {
-    return 'Mostra todos usuarios';
+  TodosUsuarios(): Pessoa[] {
+    return this.pessoaService.TodosUsuarios();
+  }
+
+  @Get(':id') 
+  UmUsuario(@Param() pessoas): Pessoa {
+    return this.pessoaService.UmUsuario(pessoas.id)
   }
 
   @Post()
-  cadastro(@Body() pessoas): string {
-    return `novo criar cadastro ${pessoas}`
+  Cadastro(@Body() pessoas: Pessoa) {
+    this.pessoaService.Criar(pessoas)
+    return 'salvo com sucesso'
   }
 
   @Put()
-  editar(@Body() pessoas): string {
-    return `editar cadastro ${pessoas   }`
+  Editar(@Body() pessoas: Pessoa): Pessoa {
+    return this.pessoaService.Editar(pessoas)
   }
 
   @Delete(':id')
-  delete(@Param() pessoas): string {
-    return `Deletar do banco ${pessoas.id}`
+  Delete(@Param() pessoas){
+    return this.pessoaService.Deletar(pessoas.id)
   }
 }
